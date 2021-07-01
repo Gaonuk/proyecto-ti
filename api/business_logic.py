32814d7d4f98ff19d6df6275f43d5ba617f236f4
Benjamin Aldana <baaldana@uc.cl>
2021-07-01 12:31:22 -0400

    se agregan prints para seguir la lógica

--- api/business_logic.py
+++ api/business_logic.py
@@ -1,5 +1,6 @@
 from .models import Lote, Pedido, ProductoBodega, ProductoDespachado
 from datetime import datetime
+from .warehouse import fabricar_producto
 
 SKU_VACUNAS = ['10001']
 
@@ -8,7 +9,7 @@ TIEMPOS_PRODUCCION = {
 }
 
 def factibildad(sku, cantidad_solicitada, fecha_entrega):
-    
+    print(f'{datetime.now()}: Análisis de factibilidad\n{sku} - {cantidad_solicitada} - {fecha_entrega}')
     if sku not in SKU_VACUNAS:
         # Es un producto normal
         lotes = Lote.objects.filter(
@@ -30,19 +31,30 @@ def factibildad(sku, cantidad_solicitada, fecha_entrega):
         for pedido in pedidos:
             cantidad_productos_pedidos += pedido.cantidad
         
+        cantidad_productos_total = cantidad_productos_lotes + cantidad_productos_pedidos
+        
+        print(f'Cantidad en almacenes: {cantidad_productos_lotes}\nCantidad en camino: {cantidad_productos_pedidos}\nTotal: {cantidad_productos_total}')
+
         if cantidad_solicitada <= cantidad_productos_lotes:
+            print(f'Usando solo stock en camino puedo satisfacer el pedido.')
             return True # Puedo hacerme cargo de la OC inmediatamente
-        elif cantidad_solicitada <= cantidad_productos_lotes + cantidad_productos_pedidos:
+        elif cantidad_solicitada <= cantidad_productos_total:
+            print(f'Usando stock + pedidos en camino puedo satisfacer el pedido.')
+            # Hay que reservar los productos con la OC correspondiente.
             return True
-        elif cantidad_solicitada > cantidad_productos_lotes + cantidad_productos_pedidos:
+        elif cantidad_solicitada > cantidad_productos_total:
             # Evalúo si alcanzo a producir lo que me falta
             tiempo_prod = TIEMPOS_PRODUCCION[sku]
             delta = 10
-            if fecha_entrega > datetime.now() + datetime.min(tiempo_prod + delta):
+            if fecha_entrega < datetime.now() + datetime.min(tiempo_prod + delta):
+                print(f'No alcanzo a entregar en esa fecha, porque no me llegan los pedidos.')
                 return False
             else:
-                # 
+                # Si me alcanza el tiempo debo pedir más 
+                print(f'Debo pedir {cantidad_productos_total-cantidad_solicitada} de {sku} para satisfacer la OC.')
+                return True
 
     else:
         # Es una vacuna y requiere fabricación entre medio
+        print(f'Este sku {sku} es una vacuna, y aún no está implementado su manejo')
         return False
