b092777b22dce8a3450045b5eb0d6b51103898f3
Benjamin Aldana <baaldana@uc.cl>
2021-07-20 09:10:05 -0400

    Se agrega el nuevo modelo a admin

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import SentOC, RecievedOC, Log,ProductoBodega, Pedido, ProductoDespachado
+from .models import SentOC, RecievedOC, Log,ProductoBodega, Pedido, ProductoDespachado, CantidadMaxAceptada
 # Register your models here.
 admin.site.register(SentOC)
 admin.site.register(RecievedOC)
@@ -7,3 +7,4 @@ admin.site.register(Log)
 admin.site.register(ProductoBodega)
 admin.site.register(Pedido)
 admin.site.register(ProductoDespachado)
+admin.site.register(CantidadMaxAceptada)
